841a3ae81ef8f2864c8df6bfd45c58cc77de4d98
johan <jhimelymendoza@gmail.com>
2022-01-12 18:15:28 -0300

    chore: add logic skeleton

---
+++ index.js
@@ -0,0 +1,43 @@
+   class Calculator {
+  
+    num1;
+    num2;
+    operation;
+  
+    constructor(){}
+  
+    setNum1(num1){
+       // todo aki va la logica para setar el primer operando
+    }
+  
+    setNum2(num1){
+        // todo aki va la logica para setar el segundo operando
+    }
+  
+    setOperation(operation){
+         // todo aki va la logica para setar la operacion
+    }
+
+    add() {
+         // todo aki va la logica para sumar
+    }
+  
+    rest() {
+        // todo aki va la logica para sumar
+    }
+  
+    multiplication() {
+        // todo aki va la logica para sumar
+    }
+  
+    divition() {
+        // todo aki va la logica para sumar
+    }
+  
+    print(){
+       // todo aki va la logica para mostrar las operaciones y el resultado
+    }
+  }
+
+    const car= new Calculator();
+ 
