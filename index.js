8efa2c1a43b74e120322d62b4d693aba36046a3b
jose <bonaldyjm@gmail.com>
2022-01-17 16:51:08 -0300

    pequeños cambios, aun sin poner a funcionar la calculadora

--- index.js
+++ index.js
@@ -1,43 +1,69 @@
-   class Calculator {
+
+  class Calculator {   
   
     num1;
     num2;
     operation;
   
-    constructor(){}
+    constructor(){
+
+    }
+     
+    
   
     setNum1(num1){
        // todo aki va la logica para setar el primer operando
+   this.num1 = num1.toString()
+
     }
   
-    setNum2(num1){
+    setNum2(num2){
         // todo aki va la logica para setar el segundo operando
+        this.num2 = num2.toString()
+        
     }
   
     setOperation(operation){
          // todo aki va la logica para setar la operacion
+    this.operation = operation;
+
     }
+
+
 
     add() {
          // todo aki va la logica para sumar
+      return this.num1 + this.num2
     }
   
     rest() {
         // todo aki va la logica para sumar
+        return this.num1 - this.num2
     }
   
     multiplication() {
         // todo aki va la logica para sumar
+        return this.num1 * this.num2
     }
   
     divition() {
         // todo aki va la logica para sumar
+       return this.num1 / this.num2
     }
   
     print(){
        // todo aki va la logica para mostrar las operaciones y el resultado
+      if (setoperation === "+") return this.add
+      else if (this.operation)
+
     }
   }
 
     const car= new Calculator();
- 
+
+    function pantallazo() {
+      if (document.calculator.ans.)
+    } 
+
+
+   
