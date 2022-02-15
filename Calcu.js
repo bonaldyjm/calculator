b6b1053be206ba5f71f52c3c81b809f4c45faccf
jose <bonaldyjm@gmail.com>
2022-02-15 13:32:38 -0300

    version mejorada

--- Calcu.js
+++ Calcu.js
@@ -7,10 +7,10 @@ class Calculadora {
         this.num2 = "";
         this.operator = "";
     }
-    setValorAnterior (num1) {
+    setNum1 (num1) { 
         this.num1 += num1.toString();
     }
-    setValorActual (num2) {
+    setNum2 (num2) {
         this.num2 += num2.toString();
     }
     setOperator (operator) {
@@ -20,21 +20,46 @@ class Calculadora {
     
   
     suma (num1,num2) {
-        var num = parseInt(num1)
-        var otherNum = parseInt(num2)
+      if (isNaN(num1) || isNaN(num2)) {
+        return "entrada no valida"
+      }
+
+      if (num1.includes('.') || num2.includes('.')) {
+        var num = parseFloat(num1)
+        var otherNum = parseFloat(num2)
         return num + otherNum;
+      }
+          var num = parseInt(num1)
+          var otherNum = parseInt(num2)
+          return num + otherNum;
     }
     resta (num1,num2) {
+      if (num1.includes('.') || num2.includes('.')) {
+        var num = parseFloat(num1)
+        var otherNum = parseFloat(num2)
+        return num - otherNum;
+      }
         var num = parseInt(num1)
         var otherNum = parseInt(num2)
         return num - otherNum;
     }
+    
     division (num1,num2) {
+      if (num1.includes('.') || num2.includes('.')) {
+        var num = parseFloat(num1)
+        var otherNum = parseFloat(num2)
+        return num / otherNum;
+      }
         var num = parseInt(num1)
         var otherNum = parseInt(num2)
         return num / otherNum;
     }
     multiplicacion (num1,num2) {
+      if (num1.includes('.') || num2.includes('.')) {
+        var num = parseFloat(num1)
+        var otherNum = parseFloat(num2)
+        return num * otherNum;
+      }
         var num = parseInt(num1)
         var otherNum = parseInt(num2)
         return num * otherNum;
@@ -49,17 +74,23 @@ const calculadora = new Calculadora ();
 
  
  function printOnScreen (num)  { 
-   if (!calculadora.operator) {
+  if (parseInt(document.getElementsByClassName("val")[0].innerHTML) > 999999999999999 
+  || parseInt(document.getElementsByClassName("val")[1].innerHTML) > 999999999999999) {
+    deleteApp();
+    document.getElementsByClassName("val")[0].innerHTML = "Error";
+}
+  
+  if (!calculadora.operator) {
 
    document.getElementsByClassName("val")[0].innerHTML += num;
-   calculadora.setValorAnterior(num)
+   calculadora.setNum1(num)
   }
-  if (calculadora.operator) {
-    document.getElementsByClassName("val")[1].innerHTML = " "
-    calculadora.setValorActual(num);
-    document.getElementsByClassName("val")[1].innerHTML += num;
+  if (calculadora.num1 && calculadora.operator ) {
+    document.getElementsByClassName("val")[1].innerHTML = calculadora.num2
+    document.getElementsByClassName("val")[1].innerHTML +=num;
+    calculadora.setNum2(num);
   }
- }
+}
 
 
  
@@ -70,28 +101,55 @@ function deleteApp () {
 
 }
 
+function deleteCaracter() {
+  if (document.getElementsByClassName("val")[0].innerHTML && !document.getElementsByClassName("val")[1].innerHTML) {
+    document.getElementsByClassName("val")[0].innerHTML = document.getElementsByClassName("val")[0].innerHTML.slice(0,-1);
+    calculadora.num1 = document.getElementsByClassName("val")[0].innerHTML;
+  }
+  else if (document.getElementsByClassName("val")[0].innerHTML && document.getElementsByClassName("val")[1].innerHTML){
+    document.getElementsByClassName("val")[1].innerHTML = document.getElementsByClassName("val")[1].innerHTML.slice(0,-1);
+    document.getElementsByClassName("val")[0].innerHTML = document.getElementsByClassName("val")[0].innerHTML
+    calculadora.num2 = document.getElementsByClassName("val")[1].innerHTML 
+  }
+  }
+
 function printOnScreenOp (operator) {
   document.getElementsByClassName("val")[0].innerHTML = calculadora.num1 + " " + operator;
   document.getElementsByClassName("val")[1].innerHTML = calculadora.num1;
    calculadora.setOperator(operator)
      }
 
+     function accumulated (value) {
+       if (value) {
+         calculadora.num1 = value;
+         calculadora.num2 = " ";
+         printOnScreen(operator);
+       }
+     }
 
 function botonIgual () {
 if (calculadora.operator === "+") {
   document.getElementsByClassName("val")[0].innerHTML = calculadora.num1 + " " + calculadora.operator + " " + calculadora.num2 + " =";
   document.getElementsByClassName("val")[1].innerHTML = calculadora.suma(calculadora.num1,calculadora.num2);
+  accumulated (document.getElementsByClassName("val")[1].innerHTML)
 }
 else if (calculadora.operator === "-") {
     document.getElementsByClassName("val")[0].innerHTML=  calculadora.num1 + " " + calculadora.operator + " " + calculadora.num2 + " =";
     document.getElementsByClassName("val")[1].innerHTML = calculadora.resta(calculadora.num1,calculadora.num2);
+    accumulated (document.getElementsByClassName("val")[1].innerHTML)
 }
 else if (calculadora.operator === "*") {
   document.getElementsByClassName("val")[0].innerHTML=  calculadora.num1 + " " + calculadora.operator + " " + calculadora.num2 + " =";
   document.getElementsByClassName("val")[1].innerHTML = calculadora.multiplicacion(calculadora.num1,calculadora.num2);
+  accumulated (document.getElementsByClassName("val")[1].innerHTML)
 }
 else if (calculadora.operator === "÷") {
+  if (calculadora.num2 ==="0") {
+    "ERROR"
+  } 
   document.getElementsByClassName("val")[0].innerHTML=  calculadora.num1 + " " + calculadora.operator + " " + calculadora.num2 + " =";
   document.getElementsByClassName("val")[1].innerHTML = calculadora.division(calculadora.num1,calculadora.num2);
+  accumulated (document.getElementsByClassName("val")[1].innerHTML)
+  
 }
 }
